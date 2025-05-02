55671d09cf84ebd9bcceaaccceebb06cf450af3b
tacticalcode <tacticalcodenotes@gmail.com>
2025-05-02 17:48:49 +0530

    update webhook

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -7,9 +7,9 @@ export const clerkWebhooks = async (req, res)=>{
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
     await whook.verify(JSON.stringify(req.body), {
-      "svix-id": req.headers[svix-id],
-      "svix-timestamp": req.headers[svix-timestamp],
-      "svix-signature": req.headers[svix-signature],
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
     })
 
     const {data, type} = req.body
