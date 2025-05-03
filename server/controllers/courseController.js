0dd7c7c9e60e63b300da8f2ea3114270eabf8912
tacticalcode <tacticalcodenotes@gmail.com>
2025-05-03 19:18:46 +0530

    stripe added

---
+++ server/controllers/courseController.js
@@ -0,0 +1,34 @@
+import Course from "../models/Course.js";
+//get all courses
+export const getAllCourse = async (req, res) =>{
+  try{
+    const courses = await Course.find({isPublished:true}).select(['-courseContent','-enrolledStudent']).populate({path: 'educator'})
+
+    res.json({success: true, courses})
+  } catch (error){
+    res.json({success: false, message: error.message})
+  }
+}
+
+//get course by id
+export const getCourseId = async (req, res) =>{
+  const {id} = req.params
+  try {
+    const courseData = await Course.findById(id).populate({path: 'educator'})
+
+    //remove lecture url if preview is false
+    courseData.courseContent.forEach(chapter => {
+      chapter.chapterContent.forEach(lecture => {
+        if(!lecture.isPreviewFree){
+          lecture.lectureUrl = "";
+        }
+      })
+    })
+
+    res.json({success: true, courseData})
+  } catch (error) {
+    res.json({success: false, message: error.message})
+  
+  }
+}
+
